043391aa0756833e216ebb9a114be19fe25dc5e6
Alesya Branavitskaya <alesun4ik@gmail.com>
2022-03-21 14:59:01 +0300

    feat: -create classes

--- UI/script.js
+++ UI/script.js
@@ -1,4 +1,16 @@
-// "strict": "error"
+/* eslint-disable consistent-return */
+/* eslint-disable no-setter-return */
+/* eslint-disable no-shadow */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-undef */
+/* eslint-disable max-len */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-unused-vars */
+/* eslint max-classes-per-file: ["error", 3] */
+/* eslint-disable lines-around-directive */
+/* eslint-disable strict */
+'use strict';
 
 const tweets = [
   {
@@ -262,112 +274,243 @@ const tweets = [
       }],
   },
 ];
+class Comment {
+  constructor(id, text, createdAt, author) {
+    this._id = String(Math.ceil((Math.random(new Date(0))) * 1000));
+    this.text = text;
+    this._createdAt = createdAt;
+    this._author = author;
+  }
 
-// eslint-disable-next-line no-unused-vars
-const myModule = (() => {
-  let lastId;
-  let lastCom;
-  let user = 'Алеся Брановицкая';
-  function getTweets(skip = 0, top = 10, filterConfig = {}) {
+  get id() {
+    return this._id;
+  }
+
+  set id(id) {
+    console.log('can not change', id);
+  }
+
+  get createdAt() {
+    return this._createdAt;
+  }
+
+  set createdAt(createdAt) {
+    console.log('can not change', createdAt);
+  }
+
+  get author() {
+    return this._author;
+  }
+
+  set author(author) {
+    console.log('can not change', author);
+  }
+
+  static maxTextLength() {
+    return 280;
+  }
+
+  static validate(com) {
+    if (typeof com.id === 'string' && com.text.length <= 280
+    && typeof com.text === 'string' && toString.call(com.createdAt) === '[object Date]'
+    && typeof com.author === 'string') {
+      return true;
+    }
+    return false;
+  }
+}
+
+// const comment = new Comment('1', '2', new Date('2022-02-19T19:30:00'), 'Alesya');
+// comment.id = 5;
+// comment.createdAt = 5;
+// comment.author = 5;
+// console.log(comment.id, comment.createdAt, comment.author);
+// console.log(Comment.validate({
+//   id: '904',
+//   text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type #make #intro',
+//   createdAt: new Date('2022-02-22T17:30:00'),
+//   author: 'Алеся Брановицкая',
+// }));
+
+class Tweet {
+  constructor(id, text, createdAt, author, comments) {
+    this._id = String(Math.ceil((Math.random(new Date(0))) * 1000));
+    this.text = text;
+    this._createdAt = createdAt;
+    this._author = author;
+    this.comments = comments;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  set id(id) {
+    console.log('can\'t change', id);
+  }
+
+  get createdAt() {
+    return this._createdAt;
+  }
+
+  set createdAt(createdAt) {
+    console.log('can not change', createdAt);
+  }
+
+  get author() {
+    return this._author;
+  }
+
+  set author(author) {
+    console.log('can not change', author);
+  }
+
+  addComment(id, text) {
+    const comment = new Comment(id, text, new Date(), 'Alesya');
+    if (Comment.validate(comment)) {
+      this.comments.push(comment);
+      return true;
+    }
+    return false;
+  }
+
+  static maxTextLength() {
+    return 280;
+  }
+
+  static validate(tw) {
+    if (typeof tw.id === 'string' && tw.text.length <= Tweet.maxTextLength()
+    && typeof tw.text === 'string' && toString.call(tw.createdAt) === '[object Date]'
+    && typeof tw.author === 'string' && tw.comments instanceof Array) {
+      return true;
+    }
+    return false;
+  }
+}
+
+// const tweet = new Tweet('1', '2', new Date('2022-02-19T19:30:00'), 'Alesya', []);
+// console.log(tweet);
+// tweet.id = 5;
+// tweet.createdAt = 5;
+// tweet.author = 5;
+// console.log(tweet.id, tweet.createdAt, tweet.author);
+// console.log(tweet.addComment(5, 'hello'), tweet);
+// console.log(Tweet.validate({
+//   id: '1',
+//   text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type #make #intro #datamola',
+//   createdAt: new Date('2022-02-16T03:30:00'),
+//   author: 'Алеся Брановицкая',
+//   comments: [
+//     {
+//       id: '901',
+//       text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type #make #intro',
+//       createdAt: new Date('2022-02-17T05:30:00'),
+//       author: 'Анджелина Джоли',
+//     },
+//     {
+//       id: '902',
+//       text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type #make #intro',
+//       createdAt: new Date('2022-02-18T15:30:00'),
+//       author: 'Анджелина Джоли',
+//     },
+//     {
+//       id: '903',
+//       text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type #make #intro',
+//       createdAt: new Date('2022-02-19T19:30:00'),
+//       author: 'Анджелина Джоли',
+//     }],
+// }));
+
+class TweetCollection {
+  constructor(tws) {
+    this._tws = new Set(tws);
+    this._user = 'Алеся Брановицкая';
+  }
+
+  get user() {
+    return this._user;
+  }
+
+  set user(_user) {
+    if (this._user) {
+      return this._user;
+    }
+    this._user = 'Гость';
+  }
+
+  getPage(skip = 0, top = 10, filterConfig = {}) {
     let array = [];
-    // eslint-disable-next-line no-param-reassign
     filterConfig.author = filterConfig.author ? filterConfig.author : '';
-    // eslint-disable-next-line no-param-reassign
     filterConfig.dateFrom = filterConfig.dateFrom ? new Date(filterConfig.dateFrom) : null;
-    // eslint-disable-next-line no-param-reassign
     filterConfig.dateTo = filterConfig.dateTo ? new Date(filterConfig.dateTo) : null;
-    // eslint-disable-next-line no-param-reassign
     filterConfig.text = filterConfig.text ? filterConfig.text : '';
     if (filterConfig.hashtags === undefined) {
       array = tweets;
     } else {
       for (let i = 0; i < filterConfig.hashtags.length; i += 1) {
         array = array.concat(tweets
-          .filter((tweet) => tweet.text
+          .filter((tweets) => tweets.text
             .toLowerCase().indexOf(filterConfig.hashtags[i].toLowerCase()) !== -1));
       }
     }
     return array
-      .filter((tweet) => tweet.author
+      .filter((array) => array.author
         .toLowerCase().indexOf(filterConfig.author.toLowerCase()) !== -1)
       .filter(({ createdAt }) => !((filterConfig.dateFrom && filterConfig.dateFrom > createdAt)
       || (filterConfig.dateTo && filterConfig.dateTo < createdAt)))
-      .filter((tweet) => tweet.text.toLowerCase().indexOf(filterConfig.text.toLowerCase()) !== -1)
+      .filter((array) => array.text.toLowerCase().indexOf(filterConfig.text.toLowerCase()) !== -1)
       .sort((a, b) => b.createdAt - a.createdAt)
       .slice(skip, skip + top);
   }
-  function getTweet(id) {
+
+  get(id) {
     return tweets.find((tweet) => tweet.id === String(id));
   }
-  function validateTweet(tw) {
-    if (typeof tw.id === 'string' && typeof tw.text === 'string' && tw.text.length <= 280 && toString.call(tw.createdAt) === '[object Date]' && typeof tw.author === 'string' && tw.comments instanceof Array) {
+
+  add(text) {
+    const tweet = new Tweet('5', text, new Date(), this._user, []);
+    if (Tweet.validate(tweet)) {
+      this.tws.add(tweet);
       return true;
     }
     return false;
   }
-  function addTweet(text) {
-    lastId = tweets.find((tweet) => tweet.id === tweets.length).id;
-    const item = {
-      id: String(+lastId + 1),
-      text,
-      createdAt: new Date(),
-      author: user,
-      comments: [],
-    };
-    if (validateTweet(item) && item.author === user) {
-      tweets.push(item);
-      return true;
-    }
-    return false;
-  }
-  function editTweet(id, text) {
+
+  edit(id, text) {
     const clone = tweets[id - 1];
     clone.text = text;
-    if (getTweet(id).id === String(id) && validateTweet(clone) && clone.author === user) {
+    if (this.get(id).id === String(id) && Tweet.validate(clone) && clone.author === this._user) {
       tweets[id - 1] = clone;
       return true;
     }
     return false;
   }
-  function removeTweet(id) {
-    if (getTweet(id).id === String(id) && tweets[id - 1].author === user) {
-      tweets.splice(id - 1, 1);
+
+  remove(id) {
+    if (this.get(id).id === String(id) && tweets[id - 1].author === this._user) {
+      this.tws.delete(tweets[id - 1]);
       return true;
     }
     return false;
   }
-  function validateComment(com) {
-    if (typeof com.id === 'string' && com.text.length <= 280 && typeof com.text === 'string' && toString.call(com.createdAt) === '[object Date]' && typeof com.author === 'string') {
-      return true;
-    }
-    return false;
+
+  addAll(tws) {
+    // this.tws.add(tws);
+    this.tws = new Set(tws);
   }
-  function addComment(id, text) {
-    lastCom = tweets.find((tweet) => tweet.id === String(id)).comments;
-    const item = {
-      id: String(+(lastCom[lastCom.length - 1].id) + 1),
-      text,
-      createdAt: new Date(),
-      author: user,
-    };
-    if (getTweet(id).id === String(id) && validateComment(item)) {
-      tweets[id - 1].comments.push(item);
-      return true;
-    }
-    return false;
+
+  clear() {
+    this.tws.clear();
   }
-  function changeUser(usr) {
-    user = usr;
-  }
-  return {
-    getTweets,
-    getTweet,
-    validateTweet,
-    addTweet,
-    editTweet,
-    removeTweet,
-    validateComment,
-    addComment,
-    changeUser,
-  };
-})();
+}
+
+const tweetCollection = new TweetCollection();
+// console.log(tweetCollection);
+tweetCollection.addAll(tweets);
+// console.log(tweetCollection.getPage(0, 10));
+// console.log(tweetCollection.getPage(0, 10, { hashtags: ['#datamola'] }));
+// console.log(tweetCollection.get(20));
+// console.log(tweetCollection.add('hello'), tweetCollection);
+// console.log(tweetCollection.edit(4, 'text'), tweetCollection);
+// console.log(tweetCollection.remove(1), tweetCollection);
+// tweetCollection.clear();
