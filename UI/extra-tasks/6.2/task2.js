9f428b094c41a8f8b29cccf80c31828655be12a6
Alesya Branavitskaya <alesun4ik@gmail.com>
2022-04-07 17:46:44 +0300

    fix: -fix some problems in 6.2 task

--- UI/extra-tasks/6.2/task2.js
+++ UI/extra-tasks/6.2/task2.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-continue */
+/* eslint-disable no-param-reassign */
 /* eslint-disable no-undef */
 /* eslint-disable lines-around-directive */
 /* eslint-disable strict */
@@ -6,20 +10,29 @@
 const wrapper = document.querySelector('.wrapper');
 const box = document.querySelectorAll('.box');
 
-wrapper.addEventListener('click', (event) => {
+wrapper.addEventListener('click', startGame);
+
+function startGame(event) {
   if (event.target.classList.contains('box')) {
-    event.target.innerHTML = 'x';
+    event.target.innerHTML = `<svg class="cross">
+    <line class="first" x1="7" y1="7" x2="45" y2="45" stroke="orange" stroke-width="5" stroke-linecap="round" />
+    <line class="second" x1="45" y1="7" x2="7" y2="45" stroke="orange" stroke-width="5" stroke-linecap="round" />
+    </svg>`;
+    event.target.classList.add('x');
     for (let i = 0; i < box.length; i += 1) {
       if (box[i].innerHTML === '') {
-        box[i].innerHTML = 'o';
-      } else if (box[i].innerHTML !== ''){
+        box[i].innerHTML = `<svg class="circle">
+        <circle r="22" cx="27" cy="25" stroke="blue" stroke-width="4" fill="none" stroke-linecap="round" />
+        </svg>`;
+        box[i].classList.add('o');
+      } else if (box[i].innerHTML !== '') {
         continue;
       }
       break;
     }
     checkTicTacToe();
   }
-});
+};
 
 function checkTicTacToe(event) {
   const array = [
@@ -31,16 +44,14 @@ function checkTicTacToe(event) {
     [2, 5, 8],
     [3, 4, 5],
     [6, 7, 8],
-  ]
+  ];
   for (let i = 0; i < array.length; i += 1) {
-    if (box[array[i][0]].innerHTML === 'x' && box[array[i][1]].innerHTML === 'x' && box[array[i][2]].innerHTML === 'x') {
-      alert('Вы победили');
-      // box.forEach((item) => {item.innerHTML = ''});
-    } else if (box[array[i][0]].innerHTML === 'o' && box[array[i][1]].innerHTML === 'o' && box[array[i][2]].innerHTML === 'o') {
-      alert('Вы проиграли');
-      // box.forEach((item) => {item.innerHTML = ''});
+    if (box[array[i][0]].classList.contains('x') && box[array[i][1]].classList.contains('x') && box[array[i][2]].classList.contains('x')) {
+      document.querySelector('.text').innerHTML = 'Вы выиграли';
+      wrapper.removeEventListener('click', startGame);
+    } else if (box[array[i][0]].classList.contains('o') && box[array[i][1]].classList.contains('o') && box[array[i][2]].classList.contains('o')) {
+      document.querySelector('.text').innerHTML = 'Вы проиграли';
+      wrapper.removeEventListener('click', startGame);
     }
   }
 }
-
-// не разрешать повторный клик
